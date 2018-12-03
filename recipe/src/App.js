c0957d4ce43194bc4c2c0b09be2b87d27fad3866
Shaurya Sinha <shauryasinna@gmail.com>
2018-12-03 16:27:57 -0500

    2 displays for ingredients and recipe

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -3,29 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 import AppBar from '@material-ui/core/AppBar';
 import Divider from '@material-ui/core/Divider';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <AppBar position='static'>
-        SDSDSDDS
-      </AppBar>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main-container">
+          <div className="ingredients-select">
+            Ingredients Card
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                Expansion Panel 1
+              </ExpansionPanelSummary>
+              
+              <ExpansionPanelDetails>
+                sdfsf
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </div>
+          <div className="recipe-display">Recipe Card</div>
+        
+        </div>
       </div>
+      
     );
   }
 }
