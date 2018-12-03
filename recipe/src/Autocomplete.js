34e2d450d5cdd88367499d691e24b6abea927d35
Shaurya Sinha <shauryasinna@gmail.com>
2018-12-03 18:24:43 -0500

    minor changes

--- recipe/src/Autocomplete.js
+++ recipe/src/Autocomplete.js
@@ -7,6 +7,7 @@ import Chip from '@material-ui/core/Chip';
 import keycode from 'keycode';
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
+import Button from '@material-ui/core/Button';
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
@@ -41,40 +42,155 @@ const styles = theme => ({
   });
 
 const suggestions = [
-    { label: "Afghanistan" },
-    { label: "Aland Islands" },
-    { label: "Albania" },
-    { label: "Algeria" },
-    { label: "American Samoa" },
-    { label: "Andorra" },
-    { label: "Angola" },
-    { label: "Anguilla" },
-    { label: "Antarctica" },
-    { label: "Antigua and Barbuda" },
-    { label: "Argentina" },
-    { label: "Armenia" },
-    { label: "Aruba" },
-    { label: "Australia" },
-    { label: "Austria" },
-    { label: "Azerbaijan" },
-    { label: "Bahamas" },
-    { label: "Bahrain" },
-    { label: "Bangladesh" },
-    { label: "Barbados" },
-    { label: "Belarus" },
-    { label: "Belgium" },
-    { label: "Belize" },
-    { label: "Benin" },
-    { label: "Bermuda" },
-    { label: "Bhutan" },
-    { label: "Bolivia, Plurinational State of" },
-    { label: "Bonaire, Sint Eustatius and Saba" },
-    { label: "Bosnia and Herzegovina" },
-    { label: "Botswana" },
-    { label: "Bouvet Island" },
-    { label: "Brazil" },
-    { label: "British Indian Ocean Territory" },
-    { label: "Brunei Darussalam" }
+    { label: "Egg" },
+    { label: "Milk" },
+    { label: "Parmesan" },
+    { label: "Cheese" },
+    { label: "Yogurt" },
+    { label: "Onion" },
+    { label: "Garlic" },
+    { label: "Tomato" },
+    { label: "Carrot" },
+    { label: "Bell Peppers" },
+    { label: "Broccoli" },
+    { label: "Spinach" },
+    { label: "Lettuce" },
+    { label: "Mushroom" },
+    { label: "Ginger" },
+    { label: "Avocado" },
+    { label: "Cabbage" },
+    { label: "Cauliflower" },
+    { label: "Pumpkin" },
+    { label: "Turnip" },
+    { label: "Apple" },
+    { label: "Banana" },
+    { label: "Lemon" },
+    { label: "Grape" },
+    { label: "Orange" },
+    { label: "Blackberry" },
+    { label: "Mango" },
+    { label: "Cranberry" },
+    { label: "Watermelon" },
+    { label: "Strawberry" },
+    { label: "Rice" },
+    { label: "Pasta" },
+    { label: "Bread" },
+    { label: "Oats" },
+
+    { label: "Noodle" },
+    { label: "Cornstarch" },
+    { label: "Ramen" },
+    { label: "Bagel" },
+    { label: "Hot Dog Bun" },
+    { label: "Chips" },
+    { label: "Risotto" },
+    { label: "Ravioli" },
+    { label: "Sugar" },
+    { label: "Honey" },
+    { label: "Maple Syrup" },
+    { label: "Brown Sugar" },
+    { label: "Cinnamon" },
+    { label: "Turmeric" },
+    { label: "Paprika" },
+    { label: "Cayenne" },
+    { label: "Cumin" },
+    { label: "Herbs" },
+    { label: "Chicken Breast" },
+    { label: "Ground Beef" },
+    { label: "Ham" },
+    { label: "Hot Dog" },
+    { label: "Bacon" },
+    { label: "Sausage" },
+
+    { label: "Pork Chops" },
+    { label: "Ground Turkey" },
+    { label: "Chorizo" },
+    { label: "Chicken Wing" },
+    { label: "Salami" },
+    { label: "Lamb" },
+    { label: "Sliced Turkey" },
+    { label: "Tuna" },
+    { label: "Salmon" },
+    { label: "Tilapia" },
+    { label: "Cod" },
+    { label: "Anchovies" },
+    { label: "Sardines" },
+    { label: "Mackerel" },
+
+    { label: "Halibut" },
+    { label: "Trout" },
+    { label: "Red Snapper" },
+    { label: "Shrimp" },
+    { label: "Crab" },
+    { label: "Lobster" },
+    { label: "Scallop" },
+    { label: "Prawns" },
+    { label: "Squid" },
+    { label: "Oyster" },
+    { label: "Mayonnaise" },
+    { label: "Ketchup" },
+    { label: "Mustard" },
+    { label: "Vinegar" },
+
+    { label: "Hot Sauce" },
+    { label: "Soy Sauce" },
+    { label: "Tortilla" },
+    { label: "Salsa" },
+    { label: "Tabasco" },
+    { label: "Ranch" },
+    { label: "BBQ Sauce" },
+    { label: "Sweet N Sour Sauce" },
+    { label: "Sriracha" },
+    { label: "Buffalo Sauce" },
+    { label: "Olive Oil" },
+    { label: "Vegetable Oil" },
+    { label: "Coconut Oil" },
+    { label: "Peanut Oil" },
+
+    { label: "Wasabi" },
+    { label: "Tomato Sauce" },
+    { label: "Pesto" },
+    { label: "Alfredo Sauce" },
+    { label: "Gravy" },
+    { label: "Refried Beans" },
+    { label: "Pinto Beans" },
+    { label: "Black Beans" },
+    { label: "Lentil" },
+    { label: "Kidney Beans" },
+    { label: "White Wine" },
+    { label: "Beer" },
+    { label: "Red Wine" },
+    { label: "Vodka" },
+    { label: "Rum" },
+    { label: "Whiskey" },
+    { label: "Tequila" },
+    { label: "Bourbon" },
+    { label: "Brandy" },
+    { label: "Gin" },
+    { label: "Sake" },
+    { label: "Schnapps" },
+    { label: "Broth" },
+    { label: "Almonds" },
+    { label: "Peanut Butter" },
+    { label: "Walnut" },
+    { label: "Cashew" },
+    { label: "Peanut" },
+
+    { label: "Chocolate" },
+    { label: "Apple Sauce" },
+    { label: "Jam" },
+    { label: "Marmalade" },
+    { label: "Nutella" },
+    { label: "Fudge" },
+    { label: "Pie" },
+    { label: "Pudding" },
+    { label: "Jelly" },
+    { label: "Coffee" },
+    { label: "Orange Juice" },
+    { label: "Tomato Juice" },
+    { label: "Apple Juice" },
+    { label: "Club Soda" },
+    { label: "Soda pop" }
 ];
 
 function renderSuggestion({
@@ -186,12 +302,18 @@ class DownshiftMultiple extends Component {
         return { selectedItem };
       });
     };
+
+    printIng = () => {
+        let {selectedItem} = this.state;
+        console.log(selectedItem);
+    }
   
     render() {
       const { classes } = this.props;
       const { inputValue, selectedItem } = this.state;
   
       return (
+        <div>
         <Downshift
           id="downshift-multiple"
           inputValue={inputValue}
@@ -222,9 +344,9 @@ class DownshiftMultiple extends Component {
                   )),
                   onChange: this.handleInputChange,
                   onKeyDown: this.handleKeyDown,
-                  placeholder: 'Select multiple countries',
+                  placeholder: 'Select multiple ingredients',
                 }),
-                label: 'Label',
+                label: 'Ingredient Search',
               })}
               {isOpen ? (
                 <Paper className={classes.paper} square>
@@ -242,6 +364,11 @@ class DownshiftMultiple extends Component {
             </div>
           )}
         </Downshift>
+
+        <Button variant="contained" color="primary" onClick={this.printIng}>
+            Search Recipes
+        </Button>
+        </div>
       );
     }
 }
