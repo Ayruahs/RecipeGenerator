155566f33264f12b9835487f2a7e91ecb82aae6a
Shaurya Sinha <shauryasinna@gmail.com>
2018-12-04 00:45:05 -0500

    Recipe display working

---
+++ recipe/src/RecipeGridList.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
+// import tileData from './tileData';
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'space-around',
+      overflow: 'hidden',
+      backgroundColor: theme.palette.background.paper,
+    },
+    gridList: {
+      width: 500,
+      height: 450,
+    },
+    icon: {
+      color: 'rgba(255, 255, 255, 0.54)',
+    },
+});
+
+class RecipeGridList extends Component {
+    state = {
+        tileData: []
+    }
+
+    render(){
+        const { classes } = this.props;
+       
+        return (
+        <div className={classes.root}>
+            <GridList cellHeight={180} className={classes.gridList}>
+            <GridListTile key="Subheader" cols={2} style={{ height: 'auto' }}>
+                <ListSubheader component="div">Recipes you can make</ListSubheader>
+            </GridListTile>
+            {this.props.tileData.map(tile => (
+
+                <GridListTile key={tile.label}>
+                <img src={tile.image} alt={tile.label} />
+                <GridListTileBar
+                    title={tile.label}
+                    // subtitle={<span>by: {tile.author}</span>}
+                    actionIcon={
+                    <IconButton className={classes.icon}>
+                        <InfoIcon />
+                    </IconButton>
+                    }
+                />
+                </GridListTile>
+            ))}
+            </GridList>
+        </div>
+        );
+    }
+}
+
+RecipeGridList.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeGridList);
