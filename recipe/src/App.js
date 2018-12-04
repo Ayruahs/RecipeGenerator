6206a1c65f0d3a6d2c05d3756bb3518434c42ed2
Shaurya Sinha <shauryasinna@gmail.com>
2018-12-04 01:32:09 -0500

    Style changes

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import ExpansionPanel from '@material-ui/core/ExpansionPanel';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
-import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import MultipleSearch from './Autocomplete';
 
 class App extends Component {
@@ -12,35 +8,23 @@ class App extends Component {
     return (
       <div className="App">
         <div className="ingredients-container">
+          
           <div className="ingredients-select">
-            <ExpansionPanel style={{backgroundColor:"#DFDCE3"}} >
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                Expansion Panel 1
-              </ExpansionPanelSummary>
-              
-              <ExpansionPanelDetails>
-                sdfsf
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
+            
+            <div style={{marginTop:'20%', fontSize:'300%'}}>What's in your fridge?</div>
 
             <MultipleSearch/>
-            
 
+            <div style={{marginTop:'20%', fontSize:'150%'}}>
+            Enter the ingredients you have and we'll<br/> tell you what you can make!
+            </div>
           </div>
         
         </div>
 
         <div className="recipe-container">
           <div className="recipe-display">
-            <ExpansionPanel style={{backgroundColor:"#DFDCE3"}} >
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                Expansion Panel 1
-              </ExpansionPanelSummary>
-              
-              <ExpansionPanelDetails>
-                sdfsf
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
+            Profile Info Here
           </div>
         </div>
       </div>
